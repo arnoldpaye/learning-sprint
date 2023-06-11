b005e18de1014b65fa82e271c9da44cfb6570b4e
Arnold Paye <arnoldpayet@gmail.com>
2023-06-11 10:37:34 -0400

    Service tier (#1)
    * develop a service object with spring
    
    * develop a service object with spring

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## 1. Getting Started with Spring Boot
 
 ## 2. Data Access in Spring
+
+## 3. Service Tier
--- src/main/java/com/apaye/nex/learningspring/data/ReservationRepository.java
+++ src/main/java/com/apaye/nex/learningspring/data/ReservationRepository.java
@@ -1,9 +1,11 @@
 package com.apaye.nex.learningspring.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.sql.Date;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
-  Iterable<Reservation> findReservationByResDate(Date date);
+  Iterable<Reservation> findReservationByReservationDate(Date date);
 }
